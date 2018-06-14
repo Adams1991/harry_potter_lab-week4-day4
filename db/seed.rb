ecf0c5f92efbd96df6703e68bb16088234815ec7
Adams1991 <adams121@hotmail.co.uk>
2018-06-14 12:58:48 +0100

    seed dat setup

---
+++ db/seed.rb
@@ -0,0 +1,19 @@
+require_relative('../models/hogwart_student')
+
+
+student1 = HogwartStudent.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house" => "Hufflepuff",
+  "age" => 13
+})
+
+student2 = HogwartStudent.new({
+  "first_name" => "Ron",
+  "last_name" => "Weasley",
+  "house" => "Ravenclaw",
+  "age" => 13
+})
+
+student1.save
+student2.save
